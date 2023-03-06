28164bec065aba2f1d3be6fcb1439f4d37d0a89c
Kyle <k.james.close@gmail.com>
2023-03-06 15:21:12 -0800

    unfinished. Maybe I'll return to this...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { initFormsEventHandler } from "./formHandlerDOM.js";
 import { initDirectoryButtonEventHandlers } from "./directoriesDOM";
+import { initCreateTaskButton } from "./createTask.js";
+import { initDeleteAllTasksButton } from "./deleteTask.js";
 
 export let projectList = new Array();
 export let activeProject;
@@ -7,3 +9,5 @@ export let activeList;
 
 initFormsEventHandler();
 initDirectoryButtonEventHandlers();
+initCreateTaskButton();
+initDeleteAllTasksButton();
